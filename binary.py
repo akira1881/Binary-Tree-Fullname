02232b94a64c74e141f45e2b2832c9d2e6b8ec2d
Charina Vallecera <charinavallecera08@gmail.com>
2023-01-14 18:48:11 +0800

    2nd commit

--- binary.py
+++ binary.py
@@ -4,6 +4,27 @@ class BinarySearchTreeNode:
         self.left = None
         self.right = None
 
-def add_child(self, data):
+    def add_child(self, data):
         if data == self.data:
             return # node already exist
+
+        if data < self.data:
+            if self.left:
+                self.left.add_child(data)
+            else:
+                self.left = BinarySearchTreeNode(data)
+        else:
+            if self.right:
+                self.right.add_child(data)
+            else:
+                self.right = BinarySearchTreeNode(data)
+
+    def search(self, val):
+        if self.data == val:
+            return True
+
+        if val < self.data:
+            if self.left:
+                return self.left.search(val)
+            else:
+                return False
