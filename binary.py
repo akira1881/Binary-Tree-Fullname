a1a72e6fa4b0ff6ef0b6df145334851cc3055fdc
Charina Vallecera <charinavallecera08@gmail.com>
2023-01-14 18:40:14 +0800

    1st commit

---
+++ binary.py
@@ -0,0 +1,9 @@
+class BinarySearchTreeNode:
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+
+def add_child(self, data):
+        if data == self.data:
+            return # node already exist
